8b8ce1f718b2d854e3c5f44c2ff3a72d8127bd90
Jakob Metzger <jakob.me@gmail.com>
2019-08-24 22:48:38 +0200

    feat: renames 'random-name' command to 'name' and makes default help red

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const program = require('commander');
+const colors = require('colors');
 const randomName = require('../lib/random-name');
 
 program.version('0.0.1');
 
 program
-  .command('random-name <gender> [amount]')
+  .command('name <gender> [amount]')
   .option('-m, --middle', 'add a random middle name', false)
   .description('Generate a random name for your NPC')
   .action(randomName);
@@ -13,5 +14,5 @@ program
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp();
+  program.outputHelp(text => colors.red(text));
 }
