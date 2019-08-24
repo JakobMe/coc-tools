564f6cd248eaec454f84b62af578bb9f157b62a5
Jakob Metzger <jakob.me@gmail.com>
2019-08-25 01:26:42 +0200

    feat: implements command 'phobia'

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 const program = require('commander');
 const colors = require('colors');
-const randomName = require('../lib/cmd/name');
+const package = require('../package');
+const terminal = require('../lib/util/terminal');
+const name = require('../lib/cmd/name');
+const phobia = require('../lib/cmd/phobia');
 
-program.version('0.0.1');
+program.version(package.version);
 
 program
   .command('name <gender> [number]')
   .option('-m, --middle', 'add a random middle name', false)
   .description('Generate a number of random names for a specific gender')
-  .action(randomName);
+  .action(name);
+
+program
+  .command('phobia')
+  .option('-l, --list', 'show complete list of all phobias', false)
+  .description('Pick a random phobia or show a list of all phobias')
+  .action(terminal.ignoreArgs(phobia));
 
 program.parse(process.argv);
 
