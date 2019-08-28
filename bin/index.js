a961181b358f79382228e2ab4eb3673bfe67defa
Jakob Metzger <jakob.me@gmail.com>
2019-08-28 20:26:35 +0200

    refactor: extracts commands to main and execute functions, renames directories

--- bin/index.js
+++ bin/index.js
@@ -2,50 +2,49 @@
 const program = require('commander');
 const colors = require('colors');
 const package = require('../package');
-const terminal = require('../lib/util/terminal');
-const name = require('../lib/cmd/name');
-const npc = require('../lib/cmd/npc');
-const phobia = require('../lib/cmd/phobia');
-const ocd = require('../lib/cmd/ocd');
-const roll = require('../lib/cmd/roll');
-const insanity = require('../lib/cmd/insanity');
-
-program.version(package.version);
+const terminal = require('../lib/utility/terminal');
+const nameCommand = require('../lib/command/name');
+const npcCommand = require('../lib/command/npc');
+const phobiaCommand = require('../lib/command/phobia');
+const ocdCommand = require('../lib/command/ocd');
+const rollCommand = require('../lib/command/roll');
+const insanityCommand = require('../lib/command/insanity');
 
 program
   .command('name <gender> [number]')
   .option('-m, --middle', 'add a random middle name', false)
   .description('Generate a number of random names for a specific gender')
-  .action(name);
+  .action(nameCommand.execute);
 
 program
   .command('npc [gender]')
   .description('Generate a random npc description')
-  .action(npc);
+  .action(npcCommand.execute);
 
 program
   .command('phobia')
   .option('-l, --list', 'show complete list of all phobias', false)
   .description('Pick a random phobia or show a complete list')
-  .action(terminal.ignoreArgs(phobia));
+  .action(terminal.ignoreArgs(phobiaCommand.execute));
 
 program
   .command('ocd')
   .option('-l, --list', 'show complete list of all obsessive-compulsive disorders', false)
   .description('Pick a random obsessive-compulsive disorder or show a complete list')
-  .action(terminal.ignoreArgs(ocd));
+  .action(terminal.ignoreArgs(ocdCommand.execute));
 
 program
   .command('roll')
   .description('Make a combined d10 + d100 roll')
-  .action(terminal.ignoreArgs(roll));
+  .action(terminal.ignoreArgs(rollCommand.execute));
 
 program
   .command('insanity')
   .option('-e, --extended', 'use list of extended insanities', false)
   .description('Pick a random insanity')
-  .action(terminal.ignoreArgs(insanity));
+  .action(terminal.ignoreArgs(insanityCommand.execute));
 
+program.version(package.version);
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
